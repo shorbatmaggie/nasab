10b9edacc8473dfa8e5575d86f1c0ce7acc3952a
shorbatmaggie <maggies@nyu.edu>
2025-07-11 00:12:42 -0400

    update
    chore: replaced Tailwind CDN with local CSS build via Tailwind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#588B8B",
+        accent: "#F0A7AD"
+      }
+    },
+  },
+  plugins: [],
+}
